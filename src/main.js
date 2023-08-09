c2e4def4e94d6921ccdc2a7ffb7e6e2f89ccb3b2
prathyush-cubet <Prathyush.Lal@opia.com>
2023-08-09 09:45:01 +0530

    Firebase added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
 
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAme9wAaK967P5DICdSQeilC_zbuCsw6zA",
+    authDomain: "cubet-vue-job.firebaseapp.com",
+    databaseURL: "https://cubet-vue-job-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "cubet-vue-job",
+    storageBucket: "cubet-vue-job.appspot.com",
+    messagingSenderId: "323883278659",
+    appId: "1:323883278659:web:1abec411b11e1598010ff6"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase.database();
 createApp(App).use(store).mount('#app')
