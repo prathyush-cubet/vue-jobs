0ba2ef710a918d8e49ebc1ebce83dee3529fc283
prathyush-cubet <Prathyush.Lal@opia.com>
2023-08-10 16:38:44 +0530

    Apply button done

--- src/components/JobDetails.vue
+++ src/components/JobDetails.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="currentJob">
       <h4>Job</h4>
-      <form>
+      <form @submit.prevent="apply(currentJob.key)">
         <div class="form-group">
           <label for="title">Title</label>
           <pre>{{currentJob.title}}</pre>
@@ -14,6 +14,9 @@
         <div class="form-group">
           <label for="date">Date</label>
           {{currentJob.date}}
+        </div>
+        <div class="form-group">
+          <button>Apply</button>
         </div>
       </form>
 
@@ -28,6 +31,8 @@
   </template>
 
   <script>
+  import firebase from "../firebase";
+  const db = firebase.ref("/applications");
   export default {
     name: "JobDetails",
     props: ["job"],
@@ -48,5 +53,15 @@
       this.currentJob = { ...this.job }
 
     },
+    methods: {
+      apply(key) {
+        var user =this.$store.getters.user.data;
+        var data = [];
+        data['jobId'] = key;
+        data['email'] = user.email;
+        db.push(data);
+        this.message = "Applied successfully";
+      }
+    }
   };
   </script>
